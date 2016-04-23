7ac69b550897905dfe7d0d668fc70022adc62469
dmccer <dmccer@gmail.com>
2016-04-23 13:47:44 +0800

    占比 mvp 团队总量

--- API文档工具.md
+++ API文档工具.md
@@ -16,15 +16,25 @@
 1. [注册账户](https://apiary.io)
 2. 获取自己的 [token](https://login.apiary.io/tokens), 建议新生成一个
 3. 配置环境变量 `APIARY_API_KEY` 为 `自己的 token`
-4. 安装 Ruby
+4. 安装 [Ruby](http://rubyinstaller.org/)
 5. 命令行执行  `gem install apiaryio` (如有必要加上 sudo, Windows 用户以管理员方式打开命令行，再执行)
 6. 查看是否安装成功 `apiary --help`
 
 ## 使用方法
 
 1. 注册公司 [gitlab](http://192.168.1.26) 账号
-2. 将文档方在项目目录中(如: `docs/api.apib`)
+2. 将文档放在项目目录中(如: `docs/api.apib`)，并在 [apiary.io](https://app.apiary.io) 上 `Create New API`(如：`ttyhuo`)
 3. 根据 Blueprint API 规范编写 API 文档
 4. 本地预览: `apiary preview --path="docs/api.apib" --output="docs/api.html"`, 浏览器打开 `docs/api.html`
 5. `apiary publish --path="docs/api.apib" --api-name="ttyhuo"`
 5. 其他命令见 [apiary cli](https://help.apiary.io/tools/apiary-cli/)
+
+## 硬伤
+
+1. 要翻墙！要翻墙！要翻墙！
+
+## 相关链接
+
+1. [Blueprint](https://apiblueprint.org/)
+2. [apiary.io](https://apiary.io)
+3. [Ruby](http://rubyinstaller.org/)
---
+++ sprint.js
@@ -0,0 +1,126 @@
+/**
+ * 计算 Tower 中一个 Sprint 里自己的 task 数量和时间
+ *
+ * 使用方法：
+ *
+ * 1. 打开 tower 选择具体的 sprint
+ * 2. 数一下自己的 task 在第几列，设你的 task 在第 n 列
+ * 3. 打开浏览器的开发者工具的控制台(Windows chrome 快捷键 (fn + )F12, OSX chrome 快捷键 cmd + alt + i)
+ * 4. 复制下面的代码将代码最后一个括号'()' 中的 'n=1' 替换为上面的 n 或 n=n
+ * 5. 按回车执行，弹出框或控制台中会显示结果
+ *
+ * author: Kane xiaoyunhua@ttyhuo.cn
+ */
+
+(function(col) {
+  var picker = function() {
+    // 2h
+    var REG_CASE_1 = /(\d+)h?/;
+    // 2h*3
+    var REG_CASE_2 = /(\d+)h?\*(\d+)/;
+
+    return function (s) {
+      var mc = s.match(REG_CASE_2);
+      if (mc && mc.length && mc.length === 3) {
+        return parseInt(mc[1]) * parseInt(mc[2]);
+      }
+
+      var m = s.match(REG_CASE_1);
+
+      if (m && m.length) {
+        if (m.length === 1) {
+          return parseInt(m[0]);
+        }
+
+        if (m.length === 2) {
+          return parseInt(m[1]);
+        }
+
+        return parseInt(m[0]);
+      }
+    };
+  }();
+
+  function tiper(msg) {
+    console.log(msg);
+    alert(msg);
+  }
+
+  function countAll() {
+    var $cols = $('.ui-sortable');
+    var ret = [];
+
+    $cols.each(function (index) {
+      var r = countCol(index);
+      ret.push(r);
+    });
+
+    var total = 0, count = 0, max = ret[0];
+    ret.forEach(function(r) {
+      total += r.total;
+      count += r.count;
+
+      if (max.total < r.total) {
+        max = r;
+      }
+    });
+
+    return {
+      total: total,
+      count: count,
+      ret: ret,
+      max: max
+    };
+  }
+
+  function countCol(col) {
+    var $cols = $('.ui-sortable');
+    var $col = $cols.eq(col);
+    var $raws = $col.find('.todo .raw');
+    var hours = [], total = 0;
+
+    $raws.each(function() {
+      var s = $(this).text();
+      var r = picker(s);
+
+      if (r != null) {
+        hours.push(r);
+      }
+    });
+
+    if (hours.length) {
+      total = hours.reduce(function(a, b) {
+        return a + b;
+      }, 0);
+    }
+
+    // 列的标题
+    var title = $.trim($('.title .name', $col).text()).replace(/\n/g, '');
+
+    return {
+      title: title,
+      hours: hours,
+      total: total,
+      count: $raws.length
+    };
+  }
+
+  if (typeof col !== 'number') {
+    tiper('请输入第几列');
+
+    return;
+  }
+
+  var all = countAll();
+  var r = all.ret[col - 1];
+  var taskPercentage = Math.round(r.count/all.count * 100);
+  var hourPercentage = Math.round(r.total/all.total * 100);
+  var maxTaskPercentage = Math.round(all.max.count/all.count * 100);
+  var maxHourPercentage = Math.round(all.max.total/all.total * 100);
+
+  tiper(`
+"${r.title}" 的 Task 总数: ${r.count}个(${taskPercentage}%), 总时长: ${r.total}h(${hourPercentage}%)\n
+本期 Task 总数: ${all.count}个, 总时长: ${all.total}h\n
+MVP 是 "${all.max.title}" 共 ${all.max.count}个(${maxTaskPercentage}%) Task, 耗时 ${all.max.total}h(${maxHourPercentage}%)
+    `);
+})(n=1);
